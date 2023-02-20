9280efa7301280df4ab7fdbaae8767e02c0e3fc7
Akiraz <alec.kirazian@gmail.com>
2023-02-19 19:22:29 -0500

    Adding rendering for enemy board

--- src/index.js
+++ src/index.js
@@ -4,4 +4,5 @@ import display from './modules/display';
 console.log('webpack works');
 display.createGrid();
 let testGame = game('alec');
-display.renderBoard(testGame.one);
+display.renderPlayer(testGame.one);
+display.renderEnemy(testGame.two);
--- src/modules/display.js
+++ src/modules/display.js
@@ -7,20 +7,34 @@ const display = (() => {
         for (let j = 0; j < 10; j++) {
           let box = document.createElement('div');
           box.classList.add('board-box');
-          box.id = `${j}, ${i}`;
+          box.dataset.coord = `${j}, ${i}`;
           node.appendChild(box);
         }
       }
     });   
   }
 
-  const renderBoard = (player) => {
+  const renderPlayer = (player) => {
     const board = player.board.getBoard();
     for (let x = 0; x < 10; x++) {
       for (let y = 0; y < 10; y++) {
         if (board[x][y] != null) {
-          const box = document.getElementById(`${x}, ${y}`);
+          const box = document.querySelector(`#player-one-board [data-coord = "${x}, ${y}"`);
           box.classList.add('ship');
+          if (board[x][y].hit == true) {
+            box.classList.add('hit');
+          }
+        }
+      }
+    }
+  }
+
+  const renderEnemy = (player) => {
+    const board = player.board.getBoard();
+    for (let x = 0; x < 10; x++) {
+      for (let y = 0; y < 10; y++) {
+        if (board[x][y] != null) {
+          const box = document.querySelector(`#player-two-board [data-coord = "${x}, ${y}"`);
           if (board[x][y].hit == true) {
             box.classList.add('hit');
           }
@@ -31,7 +45,8 @@ const display = (() => {
 
   return {
     createGrid,
-    renderBoard
+    renderPlayer,
+    renderEnemy
   }
 })();
 
